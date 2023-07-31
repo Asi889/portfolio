7472ac328a3b65d1568c906b6e0c49d26866dd93
asi marom <asafmarom89@gmail.com>
2023-07-31 13:38:45 +0300

    wip

--- src/services/projectData.js
+++ src/services/projectData.js
@@ -2,7 +2,7 @@ export default function projectData(){
     const projects=[
         {
             title: `Tsofen landing page`,
-            body: `Tsophen is a landing page I built in PHP in Wordpress, built for an initiative that aims to connect Israel's Arab population to meaningful jobs in tech.`,
+            body: `Tsophen is a landing page  I built Tsofen, an impactful landing page using PHP within the WordPress framework. This project served a noble initiative, connecting Israel's Arab community with meaningful tech job opportunities. The combination of PHP, WordPress, and CSS enabled a dynamic user experience while ensuring an intuitive and visually appealing interface.`,
             icons: ['PHP', 'Wordpress', 'CSS'],
             sourceCode:'https://github.com/Asi889/tsofen-landingPage',
             liveWebsite:'http://lp.tsofen.org/?fbclid=IwAR3grDQ7mACsKXS_ZOnfNYa8Mut1nMAqklQo_eA8E2wCRkR_UKYNLtX5jV8',
@@ -11,7 +11,7 @@ export default function projectData(){
         },
         {
             title: `Angular Weather App`,
-            body: `This is my first mini project I initiated while teaching myself Angular.`,
+            body: `The Angular Weather App marks the inception of my journey into the world of Angular, representing my first foray into this exciting technology as I pursued self-learning. This mini-project showcases my passion for exploring new frontiers in web development. With a keen focus on Angular, I skillfully combined JavaScript, Tailwind CSS, and other essential tools to build this immersive weather application. By harnessing the power of Angular, I created a seamless and dynamic user interface that provides real-time weather updates and an intuitive user experience.`,
             icons: ['Angular', 'JavaScript', 'Tailwind' ,'CSS'],
             sourceCode:'https://github.com/Asi889/angular-weather-app',
             liveWebsite:'https://asi-angular-weather-app.vercel.app/',
@@ -19,7 +19,7 @@ export default function projectData(){
         },
         {
             title: `Final Bootcamp Project`,
-            body: `GIG-Me, is a full C2C App, task manager app. Imagine Fiverr meets task manager, but designed for consumers and professionals. The app is built in React JS framework, and NODE JS server and with MongoDB as the database.`,
+            body: `GIG-Me, is a full C2C App, task manager app. Imagine Fiverr meets task manager, but designed for consumers and professionals.  Developed with React JS for the frontend and powered by a robust NODE JS server, GIG-Me leverages the efficiency of MongoDB as its database. This winning tech stack ensures seamless communication between users and delivers a dynamic platform for managing tasks, transforming the way consumers and professionals collaborate.`,
             icons: ['Node.js', 'MongoDB', 'React','MobX'],
             sourceCode:'https://github.com/Asi889/gigMeApp',
             liveWebsite:'',
@@ -28,7 +28,7 @@ export default function projectData(){
         },
         {
             title: `Maya landing page`,
-            body: `This is a landing page I built for the company Adam Milo in PHP in Wordpress.`,
+            body: `Maya Landing Page: Crafted as a captivating landing page for the esteemed company Adam Milo, the Maya project showcases my expertise in PHP within the WordPress framework. With a focus on delivering a compelling user experience, the landing page serves as a gateway to Adam Milo's world, providing essential information and enticing visitors to explore their offerings further.`,
             icons: ['PHP', 'WordPress', 'CSS'],
             sourceCode:'https://github.com/Asi889/maya-landingPage',
             liveWebsite:'https://landing.am-maya.com/',
@@ -36,23 +36,23 @@ export default function projectData(){
         },
         {
             title: `Chat Room`,
-            body: `This  was a task I got for an interview to make a chat room using only Vanilla JS, so I did! I saw the value in developing my Vanilla JS skills. I used Socket.IO and Express for the server and Tailwind on the CSS and deployed with AWS. `,
+            body: `The Vanilla JS Chat Room emerged from a challenging interview task that pushed me to showcase my prowess in pure JavaScript. With an unwavering determination to excel, I embraced the opportunity to enhance my skills in Vanilla JS, ultimately delivering a remarkable chat room application. Using Node.js, Socket.IO, and Express, I adeptly crafted a robust server that enables seamless real-time communication among users within the chat room. On the front side i used a very simple UI with Tailwind CSS ensuring an elegant and user-friendly interface that promotes effortless navigation and engagement. `,
             icons: ['Node.js', 'Javascript','Socket.IO','Tailwind', 'CSS'],
-            sourceCode:'https://github.com/Asi889/room-chat-app',
+            sourceCode:'',
             liveWebsite:'http://nodechat-env-1.eba-kxrknrwn.us-east-2.elasticbeanstalk.com/chat.html?username=Ness&room=General',
             imgeSource: 'https://res.cloudinary.com/dvdzjj8jo/image/upload/v1659299284/chatapp_xuhory.png'
         },
         {
             title: `Carnival Cruse`,
-            body: `Carnival Cruse is a end-to-end app built in React framework. Though I did not singlehndedly build this project, my contribution was largely fixing and adding new features to the existing project.`,
-            icons: ['JavaScript','React', 'State-Managment','CSS'],
+            body: `An end-to-end app developed using the React framework, Carnival Cruise exemplifies my expertise in JavaScript, React, Redux, and CSS. While I collaborated with a team on this project, my substantial contributions primarily involved debugging, refining, and implementing exciting new features to enhance the app's functionality and user experience.`,
+            icons: ['JavaScript','React', 'Redux','CSS'],
             sourceCode:'https://github.com/Asi889/frontcarnivalwhitelabel',
             liveWebsite:'https://betabundles.com/',
             imgeSource: 'https://res.cloudinary.com/dvdzjj8jo/image/upload/v1639134535/carnival_nwjjst.png'
         },
         {
             title: `UNICORN DESIGN`,
-            body: `UNICORN DESIGN is a website a website I made for a web designer client.`,
+            body: `UNICORN DESIGN stands out as an exceptional website I crafted for a valued web designer client. Leveraging the powerful combination of WordPress, PHP, JS, and Tailwind, I designed a remarkable online platform that perfectly encapsulates the client's creative vision. for this project i created a theme from scratch using carbon fileds which is a WordPress custom fileds library optimized for developers. A key aspect of my development approach is ensuring that all my projects are fully responsive, effortlessly adapting to diverse screens and devices. `,
             icons: ['PHP','WordPress','Javascript','Tailwind','CSS', 'HTML'],
             sourceCode:'',
             liveWebsite:'https://uniqorn-design.adamshriki.co.il/',
@@ -60,7 +60,7 @@ export default function projectData(){
         },
         {
             title: `Project Maya - Adam Milo `,
-            body: `A large-scale, year-long project. Project Maya is a full end-to-end system built in React Next.js framework for front end, and PHP in the backend. Throughout this project I worked in collaboration with two other programmers and other stakeholders.`,
+            body: `A large-scale, year-long project. Project Maya is a full end-to-end systeProject Maya stands as a comprehensive end-to-end system, skillfully crafted with React Next.js for the front end and backed by the robust PHP framework. This ambitious undertaking involved working and collaboration in a team of two other talented programmers and various stakeholders. Leveraging an impressive tech stack comprising PHP, JavaScript, React Next.js, state management, CSS, and Tailwind, Project Maya delivers an unparalleled user experience, marking a significant milestone in web development and fostering efficient teamwork.`,
             icons: ['PHP', 'JavaScript','React Next.js', 'State-Managment', 'CSS','Tailwind'],
             sourceCode:'https://github.com/Asi889/maya-front',
             liveWebsite:'https://am-maya.com/',
@@ -69,7 +69,7 @@ export default function projectData(){
         },
         {
             title: `Wordle in Hebrew`,
-            body: `Wordle is a viral game that I built after getting caught up in the hype. It's my first project in Vue.js. I decided to build the app to work on my skills - and I really enjoyed building it.`,
+            body: `Wordle is a viral game I built during the hype of its popularity. It's my first project in Vue.js. I decided to build the app to work on my skills, learn new technologies - and I really enjoyed the development process.`,
             icons: ['JavaScript','Vue.js','tailwind','CSS'],
             sourceCode:'https://github.com/Asi889/wordel-game',
             liveWebsite:'https://wordel-game.vercel.app/',
@@ -77,7 +77,7 @@ export default function projectData(){
         },
         {
             title: `What's 4 Dinner`,
-            body: `Don't know what to order? Whats4Dinner is a random restaurant selector using the Wolt and 10Bis restaurant list that will choose a random restaurant for you when you're feeling indecisive.`,
+            body: `Don't know what to order? Whats4Dinner is a smart random restaurant selector designed to rescue you from indecision when choosing where to order your meal. Developed with an exceptional tech stack, this web application integrates the restaurant lists from Wolt and 10Bis. Through the power of these cutting-edge technologies, the app presents you with a delightful, spontaneous dining choice, making the decision-making process a breeze.`,
             icons: ['Typscript', 'JavaScript','React','Next.js','Tailwind','CSS'],
             sourceCode:'https://github.com/Asi889/restaurant-picker',
             liveWebsite:'https://www.whats4dinner.co.il/',
