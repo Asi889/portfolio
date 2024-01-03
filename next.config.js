0a86bf80508eb5d5b51bf251fc810468bc5961be
asi marom <asafmarom89@gmail.com>
2024-01-03 19:39:10 +0200

    letgo

--- next.config.js
+++ next.config.js
@@ -1,3 +1,28 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: false,
+  swcMinify: true,
+  future: {
+    webpack5: true, // by default, if you customize webpack config, they switch back to version 4.
+    // Looks like backward compatibility approach.
+  },
+  webpack(config) {
+    config.resolve.fallback = {
+      ...config.resolve.fallback, // if you miss it, all the other options in fallback, specified
+      // by next.js will be dropped. Doesn't make much sense, but how it is
+      fs: false, // the solution
+    };
+
+    return config;
+  },
+  // pageExtensions: ['index.jsx']
+  reactStrictMode: true,
+  images: {
+    domains: ["res.cloudinary.com"],
+  },
+};
+
+module.exports = nextConfig;
 // /** @type {import('next').NextConfig} */
 // const nextConfig = {
 //   reactStrictMode: false,
@@ -16,31 +41,6 @@
 //     return config;
 //   },
 //   // pageExtensions: ['index.jsx']
-//   reactStrictMode: true,
-//   images: {
-//     domains: ["res.cloudinary.com"],
-//   },
-// };
+// }
 
-//   module.exports = nextConfig;
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: false,
-  swcMinify: true,
-  future: {
-    webpack5: true, // by default, if you customize webpack config, they switch back to version 4. 
-    // Looks like backward compatibility approach.
-  },
-  webpack(config) {
-    config.resolve.fallback = {
-      ...config.resolve.fallback, // if you miss it, all the other options in fallback, specified
-      // by next.js will be dropped. Doesn't make much sense, but how it is
-      fs: false, // the solution
-    };
-
-    return config;
-  },
-  // pageExtensions: ['index.jsx']
-}
-
-module.exports = nextConfig
+// module.exports = nextConfig
